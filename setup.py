f2bbd1867eddaa244d295e15a9676655d4370012
Om Vaghani <omvaghani2010@gmail.com>
2026-06-08 13:29:23 +0530

    components done

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages,setup
 from typing import List
 
-HYPHON_E_DOT="-e ."
+HYPHEN_E_DOT="-e ."
 def get_requirements(file_path:str)->List[str]:
     '''
     To return the list of requirements    
@@ -11,8 +11,8 @@ def get_requirements(file_path:str)->List[str]:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
         
-        if HYPHON_E_DOT in requirements:
-            requirements.remove(HYPHON_E_DOT)
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
     
     return requirements
 
--- src/exception.py
+++ src/exception.py
@@ -15,6 +15,6 @@ class CustomException(Exception):
         
     def __str__(self):
         return self.error_message
-    
+        
 
     
